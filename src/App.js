b3404fa9dbc2647cd2d5a30db259b788a33b4516
uzmakiran15 <72978374+uzmakiran15@users.noreply.github.com>
2020-12-29 18:38:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           Hello UK first app.
           Lab is great.
+          My second attempttt--
           
         </p>
         <a
